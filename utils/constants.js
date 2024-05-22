7360dd81f1a037a843343409fd1d0fe961138f5f
Joon Shakya <joonshakya07@gmail.com>
2024-05-22 08:51:57 +0545

    MM Friday Lab B [skip ci]

--- utils/constants.js
+++ utils/constants.js
@@ -540,16 +540,28 @@ export const classRoutine = {
       day: "Fri",
       a: [
         [subjects.sem5.Crypto, "314"],
-        [subjects.sem5.MM, "317"],
+        [subjects.sem5.MM, "Lab B"],
         [subjects.sem5.SAD, "Lab C"],
         ["*", ""],
       ],
       b: [
         [subjects.sem5.Simulation, "316"],
-        [subjects.sem5.MM, "317"],
+        [subjects.sem5.MM, "Lab B"],
         [subjects.sem5.SAD, "Lab C"],
         ["*", ""],
       ],
+      // a: [
+      //   [subjects.sem5.Crypto, "314"],
+      //   [subjects.sem5.MM, "317"],
+      //   [subjects.sem5.SAD, "Lab C"],
+      //   ["*", ""],
+      // ],
+      // b: [
+      //   [subjects.sem5.Simulation, "316"],
+      //   [subjects.sem5.MM, "317"],
+      //   [subjects.sem5.SAD, "Lab C"],
+      //   ["*", ""],
+      // ],
     },
   ],
   sem4: [
