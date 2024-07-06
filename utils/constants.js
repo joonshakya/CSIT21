380d21ed9293c13f169e59d9febc71dc47477aea
Joon Shakya <joonshakya07@gmail.com>
2024-07-06 11:26:00 +0545

    Remove quiz from class routine

--- utils/constants.js
+++ utils/constants.js
@@ -574,29 +574,17 @@ export const classRoutine = {
     {
       day: "Tue",
       a: [
-        [subjects.sem5["Web Tech"], "Lab A"],
-        ["Quiz", "Watrin Hall"],
-        ["Quiz", "Watrin Hall"],
-        ["Quiz", "Watrin Hall"],
+        [subjects.sem5.DAA, "314"],
+        [subjects.sem5.MM, `318`],
+        [subjects.sem5.Simulation, "314"],
+        ["*", ""],
       ],
       b: [
-        [subjects.sem5["Web Tech"], "Lab A"],
-        ["Quiz", "Watrin Hall"],
-        ["Quiz", "Watrin Hall"],
-        ["Quiz", "Watrin Hall"],
+        [subjects.sem5.Simulation, "316"],
+        [subjects.sem5.MM, `318`],
+        [subjects.sem5.Crypto, "316"],
+        ["*", ""],
       ],
-      // a: [
-      //   [subjects.sem5.DAA, "314"],
-      //   [subjects.sem5.MM, `318`],
-      //   [subjects.sem5.Simulation, "314"],
-      //   ["*", ""],
-      // ],
-      // b: [
-      //   [subjects.sem5.Simulation, "316"],
-      //   [subjects.sem5.MM, `318`],
-      //   [subjects.sem5.Crypto, "316"],
-      //   ["*", ""],
-      // ],
     },
     {
       day: "Wed",
